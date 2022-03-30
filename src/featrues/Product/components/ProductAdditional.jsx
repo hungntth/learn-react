7b191b845e62c1852e00ef4e76369839d33fa28f
Hung Nguyen <1119thaihungnguyen@gmail.com>
2022-03-30 18:11:30 +0700

    description-detail

---
+++ src/featrues/Product/components/ProductAdditional.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ProductAdditional.propTypes = {
+    
+};
+
+function ProductAdditional(props) {
+    return (
+        <div>
+            ProductAdditional
+        </div>
+    );
+}
+
+export default ProductAdditional;
--- src/featrues/Product/pages/DetailPage.jsx
+++ src/featrues/Product/pages/DetailPage.jsx
@@ -1,8 +1,13 @@
-import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import { Box, Container, Grid, LinearProgress, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
 import AddToCardForm from '../components/AddToCardForm';
+import ProductAdditional from '../components/ProductAdditional';
+import ProductDescription from '../components/ProductDescription';
 import ProductInfo from '../components/ProductInfo';
+import ProductMenu from '../components/ProductMenu';
+import ProductReviews from '../components/ProductReviews';
 import ProductThumbnail from '../components/ProductThumbnail';
 import useProductDetail from '../hooks/useProductDetail';
 
@@ -20,17 +25,28 @@ const useStyles = makeStyles((theme) => ({
     flex: '1 1 0',
     padding: theme.spacing(1.5),
   },
+  loading:{
+    position: 'fixed',
+    top: 0,
+    left:0,
+    width: '100%'
+  }
 }));
 
 function DetailPage() {
   const classes = useStyles();
   const {
     params: { productId },
+    url,
   } = useRouteMatch();
 
   const { product, loading } = useProductDetail(productId);
   if (loading) {
-      return <Box>Loading</Box>
+      return (
+        <Box className={classes.loading}>
+          <LinearProgress />
+        </Box>
+      )
   }
 
   const handleAddToCartSubmit = (formValues) => {
@@ -51,6 +67,18 @@ function DetailPage() {
             </Grid>
           </Grid>
         </Paper>
+        <ProductMenu />
+        <Switch>
+          <Route exact path={url}>
+            <ProductDescription product={product} />
+          </Route>
+          <Route exact path={`${url}/reviews`}>
+            <ProductReviews product={product} />
+          </Route>
+          <Route exact path={`${url}/additional`}>
+            <ProductAdditional product={product} />
+          </Route>
+        </Switch>
       </Container>
     </Box>
   );
